e06279e5a8145262d6659f543beff035191a42d3
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-03-17 17:20:10 +0100

    init Tabella CoordinateB

---
+++ Models/CoordinateB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeSense.Models
+{
+    public class CoordinateB
+    {
+
+            // Chiave primariria che identifica il punto
+            [Key]
+            public long Id { get; set; }
+            //Punto
+            [Required]
+            public float Point { get; set; }
+
+    }
+}
