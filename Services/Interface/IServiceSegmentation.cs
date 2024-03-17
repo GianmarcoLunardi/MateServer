65a104413accf449ee2420117f2d9ce4178e09fb
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-03-17 14:39:20 +0100

    init Service Segmentation

---
+++ Services/Interface/IServiceSegmentation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+
+namespace MakeSense.Services.Interface
+{
+    public interface IServiceSegmentation
+    {
+        Task AddAsync(Segmentation Seg);
+        Task AddRangeAsync(IEnumerable<Segmentation> Punti);
+        Task<Segmentation> RemoveAsync(long id);
+        Task<IEnumerable<Segmentation>> GetAllAsync();
+        Task<Segmentation> FindAsync(long id);
+        Task UpdateAsync(Segmentation Punto);
+    }
+}
---
+++ Services/ServiceSegmentation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+using MakeSense.Services.Interface;
+
+namespace MakeSense.Services
+{
+    public class ServiceSegmentation : IServiceSegmentation
+    {
+        private readonly Context BaseDati;
+        public ServiceSegmentation(Context _BaseDati)
+        {
+            BaseDati = _BaseDati;
+        }
+
+        public Task AddAsync(Segmentation Seg)
+        {
+            BaseDati.Segmentations.AddAsync(Seg);
+            BaseDati.SaveChangesAsync();
+            return Task.CompletedTask;
+        }
+
+        public Task AddRangeAsync(IEnumerable<Segmentation> Punti)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Segmentation> FindAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Segmentation>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Segmentation> RemoveAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Segmentation Punto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
