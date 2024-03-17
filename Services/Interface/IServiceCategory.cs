02097eedf1d263e124a7d4864b1e892fa5849ecc
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-03-17 14:52:39 +0100

    init Service Category

---
+++ Services/Interface/IServiceCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+
+namespace MakeSense.Services.Interface
+{
+    interface IServiceCategory
+    {
+        Task AddAsync(Category cat);
+        Task AddRangeAsync(IEnumerable<Category> Cat);
+        Task<Category> RemoveAsync(long id);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> FindAsync(long id);
+        Task UpdateAsync(Category cat);
+    }
+}
---
+++ Services/ServiceCategory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+using MakeSense.Services.Interface;
+
+namespace MakeSense.Services
+{
+    public class ServiceCategory : IServiceCategory
+    {
+        readonly private Context BaseDati;
+        public ServiceCategory(Context _BaseDati)
+        {
+            BaseDati = _BaseDati;
+        }
+
+        public Task AddAsync(Category cat)
+        {
+            BaseDati.Categories.AddAsync(cat);
+            BaseDati.SaveChangesAsync();
+            return Task.CompletedTask;
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<Category> Cat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> FindAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> RemoveAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category cat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
