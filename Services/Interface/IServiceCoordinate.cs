779427b637cce83cffa0868802100fa54b27205b
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-03-17 14:16:06 +0100

    init Service Coordinate

---
+++ Services/Interface/IServiceCoordinate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+
+namespace MakeSense.Services.Interface
+{
+    public interface IServiceCoordinate
+    {
+        Task Add(Coordinate Point);
+        Task AddRangeAsync(IEnumerable<Coordinate> Punti);
+        Task<Coordinate> RemoveAsync(long id);
+        Task<IEnumerable<Coordinate>> GetAllAsync();
+        Task<Coordinate> FindAsync(long id);
+        Task UpdateAsync(Coordinate Punto);
+    }
+}
+
+/*
+ * Task AddRangeAsync(IEnumerable<BookChapter> chapters);
+Task<BookChapter> RemoveAsync(Guid id);
+Task<IEnumerable<BookChapter>> GetAllAsync();
+Task<BookChapter> FindAsync(Guid id);
+Task UpdateAsync(BookChapter chapter);
+ * 
+ * 
+ * 
+*/
---
+++ Services/ServiceCoordinate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Models;
+using MakeSense.Services.Interface;
+
+namespace MakeSense.Services
+{
+    public class ServiceCoordinate : IServiceCoordinate
+    {
+        private readonly Context BaseDati;
+
+        public ServiceCoordinate(Context baseDati)
+        {
+            BaseDati = baseDati;
+        }
+
+        public Task Add(Coordinate Point)
+        {
+            BaseDati.Coordinates.AddAsync(Point);
+            BaseDati.SaveChangesAsync();
+            return Task.CompletedTask;
+           // throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<Coordinate> Punti)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coordinate> FindAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Coordinate>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coordinate> RemoveAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Coordinate Punto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
