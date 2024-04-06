da1d01ba72b438ddd7a62dde1c08dd060d3e4525
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-04-06 18:12:04 +0200

    aggiunta tabella Registry

---
+++ Models/Registry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeSense.Models
+{
+    public class Registry
+    {
+        public Guid Id { get; set; }
+        public Image ImageId { get; set; }
+        public DateTime RegistratedOn { get; set; }
+        public string Operation { get; set; }
+        public string Description { get; set; }
+    }
+}
