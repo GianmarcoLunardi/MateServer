6de4087b27e23062913992b01527b7d3d2640bd0
GianmarcoLunardi <gianmarcofi@gmail.com>
2024-04-09 16:19:00 +0200

    Controller Image Aggiunto

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MakeSense.Services.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MakeSense.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IServiceImage service;
+
+        public ImageController(IServiceImage _service)
+        {
+            this.service = _service;
+        }
+
+        /*
+// GET: api/<ImageController>
+[HttpGet]
+public IEnumerable<string> Get()
+{
+   return new string[] { "value1", "value2" };
+}
+*/
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok("File Caricato");
+        }
+
+
+
+
+        // GET api/<ImageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        /*
+        // POST api/<ImageController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        */
+        //async Task<IActionResult>
+        // POST api/<ImageController>
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            
+            string controllo = await service.UploadFile(file);
+            return Ok(controllo);
+        }
+
+
+
+        // PUT api/<ImageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ImageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Services/ServiceImage.cs
+++ Services/ServiceImage.cs
@@ -1,18 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using MakeSense.Models;
+using MakeSense.Services.Interface;
+using Microsoft.AspNetCore.Http;
 
 namespace MakeSense.Services
 {
-    public class ServiceImage
+    public class ServiceImage : IServiceImage
     {
         private readonly Context BaseDati;
 
         public ServiceImage(Context baseDati)
         {
             BaseDati = baseDati;
+        }
+
+
+
+        /// <summary>
+        /// https://www.youtube.com/watch?v=6-FNejMrVuk
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            //false l autenticazione
+
+
+            // controlla il tipo di file
+            // se l estenzione è quella giusta 
+
+            List<string> ValidExtention = new List<string>  { ".jpg" , ".tif"};
+            string extention = Path.GetExtension(file.FileName);
+            if (!ValidExtention.Contains(extention)){
+                Console.WriteLine("ERRORE NELL ESTENZIONE DEI FILE");
+
+               // Console.WriteLine("------Ci siamooooooo-----JPGGGGGGGGGGG");
+
+                return   "con estenzione non possibile"; }
+            // controlla la dimenzione
+            long lunghezza = file.Length;
+            if (lunghezza > (10 * 1024 * 1024))
+            {
+                return " dimenzoione superiore a 10Mb";
+            }
+            
+            // controlla 
+
+
+            // cambia nome
+
+            string NomeFile = "Foto" + Guid.NewGuid().ToString() + extention;
+
+            // percorso file
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Foto");
+            using FileStream stram = new FileStream(Path.Combine (path , NomeFile), FileMode.Create);
+            file.CopyTo(stram);
+            Console.WriteLine("------Ci siamooooooo-----File Copiato " + path+NomeFile);
+            return NomeFile;
+            //throw new NotImplementedException();
         }
     }
 }
